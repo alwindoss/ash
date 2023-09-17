564e4ef07651d2abdcfa26b039248a161b401058
Alwin Doss <alwindoss84@gmail.com>
2023-09-17 18:50:17 +0530

    Initial shell
    Signed-off-by: Alwin Doss <alwindoss84@gmail.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alwindoss/ash
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Ash> ")
+		ln, _, err := reader.ReadLine()
+		if err != nil {
+			fmt.Println("Error", err)
+			return
+
+		}
+		if string(ln) == "q" {
+			break
+		}
+		fmt.Println("You typed", string(ln))
+	}
+	fmt.Println("Exiting")
+}
